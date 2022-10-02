86f4f91fd984e5ffc57108ec6e6446b990614796
myepark <myepark@gmarket.com>
2022-10-02 23:48:14 +0900

    router, store 공부 중

---
+++ src/router/routes.ts
@@ -0,0 +1,19 @@
+//
+// import { RouteConfig }     from "vue-router/types/router";
+// const Create = () => import('@/SellerWebform/src/pages/Create.vue');
+// const Management = () => import('@/SellerWebform/src/pages/Management.vue');
+//
+// const routes:Array<RouteConfig> = [
+//   {
+//     path: '/Create',
+//     name: 'Create',
+//     component: Create
+//   },
+//   {
+//     path: '/Manage',
+//     name: 'Management',
+//     component: Management
+//   }
+// ]
+//
+// export default routes;
